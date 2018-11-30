ab4e98266d4155e326b683676ada794ada46fbe5
Shinyi Chen <jchen@isi.edu>
2018-11-30 14:49:07 -0800

    Formatted to google java format

--- gigaword-indexer/src/main/java/edu/isi/vista/gigawordIndexer/GigawordApplicationContextInitializer.java
+++ gigaword-indexer/src/main/java/edu/isi/vista/gigawordIndexer/GigawordApplicationContextInitializer.java
@@ -12,44 +12,41 @@ import org.springframework.core.io.support.ResourcePropertySource;
 import de.tudarmstadt.ukp.clarin.webanno.support.SettingsUtil;
 import de.tudarmstadt.ukp.clarin.webanno.support.logging.LoggingFilter;
 
-public class GigawordApplicationContextInitializer 
-	implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-	
-	public static final String PROFILE_PREAUTH = "auto-mode-preauth";
-    public static final String PROFILE_DATABASE = "auto-mode-builtin";
+public class GigawordApplicationContextInitializer
+    implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
-    private static final String AUTH_MODE_PREAUTH = "preauth";
+  public static final String PROFILE_PREAUTH = "auto-mode-preauth";
+  public static final String PROFILE_DATABASE = "auto-mode-builtin";
 
-	@Override
-	public void initialize(ConfigurableApplicationContext aApplicationContext) {
-		LoggingFilter.setLoggingUsername("SYSTEM");
-        
-        ConfigurableEnvironment aEnvironment = aApplicationContext.getEnvironment();
+  private static final String AUTH_MODE_PREAUTH = "preauth";
 
-        File settings = SettingsUtil.getSettingsFile();
-        
-        // If settings were found, add them to the environment
-        if (settings != null) {
-            System.out.println("Settings: " + settings);
-            try {
-                aEnvironment.getPropertySources().addFirst(
-                        new ResourcePropertySource(new FileSystemResource(settings)));
-            }
-            catch (IOException e) {
-                throw new IllegalStateException(e);
-            }
-        }
+  @Override
+  public void initialize(ConfigurableApplicationContext aApplicationContext) {
+    LoggingFilter.setLoggingUsername("SYSTEM");
 
-        // Activate bean profile depending on authentication mode
-        if (AUTH_MODE_PREAUTH.equals(aEnvironment.getProperty(SettingsUtil.CFG_AUTH_MODE))) {
-            aEnvironment.setActiveProfiles(PROFILE_PREAUTH);
-            System.out.println("Authentication: pre-auth");
-        }
-        else {
-            aEnvironment.setActiveProfiles(PROFILE_DATABASE);
-            System.out.println("Authentication: database");
-        }
-		
-	}
+    ConfigurableEnvironment aEnvironment = aApplicationContext.getEnvironment();
 
+    File settings = SettingsUtil.getSettingsFile();
+
+    // If settings were found, add them to the environment
+    if (settings != null) {
+      System.out.println("Settings: " + settings);
+      try {
+        aEnvironment
+            .getPropertySources()
+            .addFirst(new ResourcePropertySource(new FileSystemResource(settings)));
+      } catch (IOException e) {
+        throw new IllegalStateException(e);
+      }
+    }
+
+    // Activate bean profile depending on authentication mode
+    if (AUTH_MODE_PREAUTH.equals(aEnvironment.getProperty(SettingsUtil.CFG_AUTH_MODE))) {
+      aEnvironment.setActiveProfiles(PROFILE_PREAUTH);
+      System.out.println("Authentication: pre-auth");
+    } else {
+      aEnvironment.setActiveProfiles(PROFILE_DATABASE);
+      System.out.println("Authentication: database");
+    }
+  }
 }
--- gigaword-indexer/src/main/java/edu/isi/vista/gigawordIndexer/GigawordIndexer.java
+++ gigaword-indexer/src/main/java/edu/isi/vista/gigawordIndexer/GigawordIndexer.java
@@ -1,7 +1,5 @@
 package edu.isi.vista.gigawordIndexer;
 
-
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -46,169 +44,163 @@ import de.tudarmstadt.ukp.clarin.webanno.ui.monitoring.page.MonitoringPageMenuIt
 
 @SpringBootApplication
 @ComponentScan(
-		basePackages={"de.tudarmstadt.ukp.inception.app",
-		"de.tudarmstadt.ukp.clarin.webanno"},
-		excludeFilters = {
-	        @Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
-	        @Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class),
-	        @Filter(type = FilterType.ASSIGNABLE_TYPE, classes = { 
-	                // The INCEpTION dashboard uses a per-project view while WebAnno uses a global
-	                // activation strategies for menu items. Thus, we need to re-implement the menu
-	                // items for INCEpTION.
-	                AnnotationPageMenuItem.class,
-	                CurationPageMenuItem.class,
-	                MonitoringPageMenuItem.class,
-	                AgreementPageMenuItem.class,
-	                // INCEpTION uses its recommenders, not the WebAnno automation code
-	                AutomationService.class, 
-	                AutomationMiraTemplateExporter.class,
-	                AutomationTrainingDocumentExporter.class
-        })}
-)
-@EntityScan(basePackages = {
-        // Include WebAnno entity packages separately so we can skip the automation entities!
-        "de.tudarmstadt.ukp.clarin.webanno.model",
-        "de.tudarmstadt.ukp.clarin.webanno.security"})
-@ImportResource({ 
-    "classpath:application-context.xml"
-})
-public class GigawordIndexer implements CommandLineRunner{
+    basePackages = {"de.tudarmstadt.ukp.inception.app", "de.tudarmstadt.ukp.clarin.webanno"},
+    excludeFilters = {
+      @Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
+      @Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class),
+      @Filter(
+          type = FilterType.ASSIGNABLE_TYPE,
+          classes = {
+            // The INCEpTION dashboard uses a per-project view while WebAnno uses a global
+            // activation strategies for menu items. Thus, we need to re-implement the menu
+            // items for INCEpTION.
+            AnnotationPageMenuItem.class,
+            CurationPageMenuItem.class,
+            MonitoringPageMenuItem.class,
+            AgreementPageMenuItem.class,
+            // INCEpTION uses its recommenders, not the WebAnno automation code
+            AutomationService.class,
+            AutomationMiraTemplateExporter.class,
+            AutomationTrainingDocumentExporter.class
+          })
+    })
+@EntityScan(
+    basePackages = {
+      // Include WebAnno entity packages separately so we can skip the automation entities!
+      "de.tudarmstadt.ukp.clarin.webanno.model",
+      "de.tudarmstadt.ukp.clarin.webanno.security"
+    })
+@ImportResource({"classpath:application-context.xml"})
+public class GigawordIndexer implements CommandLineRunner {
 
-	@Autowired 
-	@Qualifier("authenticationManager1")
-	private AuthenticationManager authManager;
+  @Autowired
+  @Qualifier("authenticationManager1")
+  private AuthenticationManager authManager;
 
-	private @Autowired UserDao userRepository;
-    
-	private @Autowired ProjectService projectService;
-    
-	private @Autowired DocumentService documentService;
-    
-	private @Autowired AnnotationSchemaService annotationSchemaService;
+  private @Autowired UserDao userRepository;
 
-    private static final String PROJECT_NAME_GW = "Gigawords";
+  private @Autowired ProjectService projectService;
 
-    public GigawordIndexer() {
-    	
+  private @Autowired DocumentService documentService;
+
+  private @Autowired AnnotationSchemaService annotationSchemaService;
+
+  private static final String PROJECT_NAME_GW = "Gigawords";
+
+  public GigawordIndexer() {}
+
+  public void run(String... args) {
+
+    System.out.println("GigawordIndexer is running...");
+
+    // TODO
+    // parse gigaword
+    // create Project
+    // create SourceDocuments
+    // import document text
+
+    // see a list of users
+    List<User> users = userRepository.list();
+    System.out.println("Users:");
+    for (User user : users) {
+      System.out.println("	" + user.getUsername() + ":" + user.getPassword());
     }
 
-    public void run(String... args) {
-    	
-    	System.out.println("GigawordIndexer is running...");
-        
-    	// TODO
-    	// parse gigaword
-        // create Project
-        // create SourceDocuments
-        // import document text
-    	
-  
-    	// see a list of users
-    	List<User> users = userRepository.list();
-    	System.out.println("Users:");
-    	for (User user : users) {
-    		System.out.println("	" + user.getUsername() + ":" + user.getPassword());
-    	}
-    	
-    	// Check if authenticated
-    	Authentication a = SecurityContextHolder.getContext().getAuthentication();
-    	System.out.println("Authentication: " + a);
-    	
-    	// Authenticate user (admin)
-    	System.out.println("Authenticating user admin... ");
-    	UsernamePasswordAuthenticationToken authReq
-    		= new UsernamePasswordAuthenticationToken("admin", "admin");
-    	Authentication auth = authManager.authenticate(authReq);
-    	SecurityContextHolder.getContext().setAuthentication(auth);
+    // Check if authenticated
+    Authentication a = SecurityContextHolder.getContext().getAuthentication();
+    System.out.println("Authentication: " + a);
 
-    	// Test current user is admin
-    	User user = userRepository.getCurrentUser();
-    	System.out.println("Current user: " + user.getUsername());
-    	
-    	// List projects
-    	System.out.println("List Projects: ");
-    	List<Project> projects = projectService.listAccessibleProjects(user);
-    	for (Project project : projects) {
-          System.out.println("	" + project.getName());   
-    	}
-    		
-        
-    	// create and delete a project
-    	try {
-    		
-    		// delete test project if already exists
-    		if (projectService.existsProject(PROJECT_NAME_GW)) {
-    			Project projectGigawords = projectService.getProject(PROJECT_NAME_GW);
-            	projectService.removeProject(projectGigawords);
-            }
-    		
-    		// create a project
-            System.out.println("Creating project " + PROJECT_NAME_GW);
-    		Project project = new Project();
-            project.setName(PROJECT_NAME_GW);
-            project.setMode(WebAnnoConst.PROJECT_TYPE_ANNOTATION);
-            projectService.createProject(project);
-            annotationSchemaService.initializeProject(project);
-            
-            // create a document
-            SourceDocument sourceDocument = new SourceDocument();
-            sourceDocument.setName("Gigaword Doc 1");
-            sourceDocument.setProject(project);
-            sourceDocument.setFormat("text");
+    // Authenticate user (admin)
+    System.out.println("Authenticating user admin... ");
+    UsernamePasswordAuthenticationToken authReq =
+        new UsernamePasswordAuthenticationToken("admin", "admin");
+    Authentication auth = authManager.authenticate(authReq);
+    SecurityContextHolder.getContext().setAuthentication(auth);
 
-            String fileContent = "The capital of Galicia is Santiago de Compostela.";
+    // Test current user is admin
+    User user = userRepository.getCurrentUser();
+    System.out.println("Current user: " + user.getUsername());
 
-            // upload document
-            InputStream fileStream = new ByteArrayInputStream(
-            		fileContent.getBytes(StandardCharsets.UTF_8));
-
-            documentService.uploadSourceDocument(fileStream, sourceDocument);
-            
-            // check project and document created
-            if (projectService.existsProject(PROJECT_NAME_GW)) {
-            	Project projectGigawords = projectService.getProject(PROJECT_NAME_GW);
-            	System.out.println("Project " + projectGigawords.getName() + " created on " + projectGigawords.getCreated());
-            
-            	System.out.println("List documents under project " + projectGigawords);
-            	List<SourceDocument> documents = documentService.listSourceDocuments(projectGigawords);
-            	Iterator<SourceDocument> itr = documents.iterator();
-            	while(itr.hasNext()) {
-            		SourceDocument doc = itr.next();
-            		System.out.println("  " + doc.getName());
-            	}
-            	
-            	// delete project
-                projectService.removeProject(projectGigawords);
-            }
-            else 
-            	System.out.println("Project " + PROJECT_NAME_GW + " not created.");
-
-            
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (UIMAException e) {
-			e.printStackTrace();
-		}
-    	
-    	
+    // List projects
+    System.out.println("List Projects: ");
+    List<Project> projects = projectService.listAccessibleProjects(user);
+    for (Project project : projects) {
+      System.out.println("	" + project.getName());
     }
 
-    public static void main(String[] args) {
-    	
-//        SpringApplication app = new SpringApplication(GigawordIndexer.class);
-//        app.setBannerMode(Banner.Mode.OFF);
-//        app.run(args);
-    	
-    	SpringApplicationBuilder builder = new SpringApplicationBuilder();
-        builder.properties("running.from.commandline=true");
-        builder.bannerMode(Banner.Mode.OFF);
-        builder.initializers(new GigawordApplicationContextInitializer());
-        builder.properties("spring.config.location="
-                + "${inception.home:${user.home}/.inception}/settings.properties");
-        builder.sources(GigawordIndexer.class);
-        builder.run(args);
+    // create and delete a project
+    try {
+
+      // delete test project if already exists
+      if (projectService.existsProject(PROJECT_NAME_GW)) {
+        Project projectGigawords = projectService.getProject(PROJECT_NAME_GW);
+        projectService.removeProject(projectGigawords);
+      }
+
+      // create a project
+      System.out.println("Creating project " + PROJECT_NAME_GW);
+      Project project = new Project();
+      project.setName(PROJECT_NAME_GW);
+      project.setMode(WebAnnoConst.PROJECT_TYPE_ANNOTATION);
+      projectService.createProject(project);
+      annotationSchemaService.initializeProject(project);
+
+      // create a document
+      SourceDocument sourceDocument = new SourceDocument();
+      sourceDocument.setName("Gigaword Doc 1");
+      sourceDocument.setProject(project);
+      sourceDocument.setFormat("text");
+
+      String fileContent = "The capital of Galicia is Santiago de Compostela.";
+
+      // upload document
+      InputStream fileStream =
+          new ByteArrayInputStream(fileContent.getBytes(StandardCharsets.UTF_8));
+
+      documentService.uploadSourceDocument(fileStream, sourceDocument);
+
+      // check project and document created
+      if (projectService.existsProject(PROJECT_NAME_GW)) {
+        Project projectGigawords = projectService.getProject(PROJECT_NAME_GW);
+        System.out.println(
+            "Project "
+                + projectGigawords.getName()
+                + " created on "
+                + projectGigawords.getCreated());
+
+        System.out.println("List documents under project " + projectGigawords);
+        List<SourceDocument> documents = documentService.listSourceDocuments(projectGigawords);
+        Iterator<SourceDocument> itr = documents.iterator();
+        while (itr.hasNext()) {
+          SourceDocument doc = itr.next();
+          System.out.println("  " + doc.getName());
+        }
+
+        // delete project
+        projectService.removeProject(projectGigawords);
+      } else System.out.println("Project " + PROJECT_NAME_GW + " not created.");
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (UIMAException e) {
+      e.printStackTrace();
     }
+  }
+
+  public static void main(String[] args) {
+
+    //        SpringApplication app = new SpringApplication(GigawordIndexer.class);
+    //        app.setBannerMode(Banner.Mode.OFF);
+    //        app.run(args);
+
+    SpringApplicationBuilder builder = new SpringApplicationBuilder();
+    builder.properties("running.from.commandline=true");
+    builder.bannerMode(Banner.Mode.OFF);
+    builder.initializers(new GigawordApplicationContextInitializer());
+    builder.properties(
+        "spring.config.location="
+            + "${inception.home:${user.home}/.inception}/settings.properties");
+    builder.sources(GigawordIndexer.class);
+    builder.run(args);
+  }
 }
-
-
-
