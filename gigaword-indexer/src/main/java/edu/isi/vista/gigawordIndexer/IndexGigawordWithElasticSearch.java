ae519a595caef0b60b8df4a571a10f0dc75f9afe
Shinyi Chen <jchen@isi.edu>
2018-12-06 14:19:15 -0800

    Elasticsearch indexing from a Gzip file.

--- gigaword-indexer/src/main/java/edu/isi/vista/gigawordIndexer/IndexGigawordWithElasticSearch.java
+++ gigaword-indexer/src/main/java/edu/isi/vista/gigawordIndexer/IndexGigawordWithElasticSearch.java
@@ -1,52 +1,83 @@
 package edu.isi.vista.gigawordIndexer;
 
-import com.google.common.collect.ImmutableMap;
-import edu.isi.nlp.parameters.Parameters;
-import org.apache.http.HttpHost;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestHighLevelClient;
-
 import java.io.File;
 import java.io.IOException;
-import java.util.Map;
+import java.nio.file.Files;
+import java.util.Iterator;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import edu.isi.vista.gigawordIndexer.GigawordFileProcessor.Article;
 
 public class IndexGigawordWithElasticSearch {
-    private static final ImmutableMap<String, String> sampleDocs = ImmutableMap.of(
-            "doc1", "Hello world",
-            "doc2", "la la la",
-            "doc3", "The music-room in the Governor’s House at Port Mahon, a tall, handsome," +
-                    " pillared octagon, was filled with the triumphant first movement of Locatelli’s C major quartet."
-    );
 
-    public static void main(String[] argv) throws IOException {
-        final Parameters parameters;
+  private static final Logger log = LoggerFactory.getLogger(IndexGigawordWithElasticSearch.class);
 
-        if (argv.length > 0) {
-            parameters = Parameters.loadSerifStyle(new File(argv[0]));
-        } else {
-            parameters = Parameters.fromMap(ImmutableMap.of());
-        }
+  /**
+   * @param argv takes two arguments, a path to the GZip file and an index name
+   * @throws IOException
+   */
+  public static void main(String[] argv) throws IOException {
 
-        try (RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost(parameters.getOptionalString("primaryHostName").or("localhost"),
-                                parameters.getOptionalPositiveInteger("primaryPort").or(9200),
-                                "http"),
-                        new HttpHost(parameters.getOptionalString("secondaryHostName").or("localhost"),
-                                parameters.getOptionalPositiveInteger("secondaryPort").or(9201),
-                                "http")
-                ))) {
-            for (Map.Entry<String, String> e : sampleDocs.entrySet()) {
-                final String docId = e.getKey();
-                final String docContent = e.getValue();
-
-                // you can index documents one-by-one
-                client.index(new IndexRequest("documents", "doc", docId)
-                        .source("text", docContent));
-            }
-
-            // TODO: in practice, you will want to use BulkRequest for speed
-        }
+    // get arguments: gzip file path and index name
+    if (argv.length < 2) {
+      log.error("Required inputs: file path to GZip file and index name");
+      System.exit(-1);
     }
+
+    String filePath = argv[0];
+    String indexName = argv[1];
+
+    // check filePath is valid
+    File file;
+    if (!(file = new File(filePath)).exists()) {
+      log.error("{} does not exist", filePath);
+      System.exit(-1);
+    }
+    String contentType = Files.probeContentType(file.toPath());
+    if ((contentType != null && !contentType.equalsIgnoreCase("application/gzip"))
+        || (contentType == null
+            && !FilenameUtils.getExtension(file.getName()).equalsIgnoreCase("gz"))) {
+      log.error("{} is not a valid GZip file", filePath);
+      System.exit(-1);
+    }
+
+    // start indexing
+    try {
+
+      RestHighLevelClient client =
+          new RestHighLevelClient(
+              RestClient.builder(
+                  new HttpHost("localhost", 9200, "http"),
+                  new HttpHost("localhost", 9201, "http")));
+
+      BulkRequest bulkRequest = new BulkRequest();
+      GigawordFileProcessor proc = new GigawordFileProcessor(file);
+      Iterator<Article> iterator = proc.iterator();
+      while (iterator.hasNext()) {
+        Article article = iterator.next();
+
+        bulkRequest.add(
+            new IndexRequest(indexName, "doc", article.getId()).source("text", article.getText()));
+      }
+
+      client.bulk(bulkRequest, RequestOptions.DEFAULT);
+
+      client.close();
+      client = null;
+
+    } catch (Exception e) {
+      log.error(e.getMessage());
+      e.printStackTrace();
+      System.exit(-1);
+    }
+  }
 }
