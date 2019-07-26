0e219ffcee224336fe5b9ca4e133bbee984bbdc3
Eric Berquist <berquist@isi.edu>
2019-07-26 16:49:07 -0400

    Only try restoring English Gigaword documents

--- annotation-utils/src/main/java/edu/isi/vista/annotationutils/RestoreJson.kt
+++ annotation-utils/src/main/java/edu/isi/vista/annotationutils/RestoreJson.kt
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import edu.isi.nlp.io.OriginalTextSource
 import edu.isi.nlp.parameters.Parameters
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
 
 /**
@@ -31,30 +33,38 @@ import java.nio.file.Paths
 
 fun main(argv: Array<String>) {
     val params = Parameters.loadSerifStyle(File(argv[0]))
-    val inputJsonDirectory = params.getCreatableDirectory("inputJsonDirectory")
-    val outputDirectory = params.getCreatableDirectory("restoredJsonDirectory")
+    val inputJsonDirectory = params.getExistingDirectory("inputJsonDirectory")!!
+    val outputDirectory = params.getCreatableDirectory("restoredJsonDirectory")!!
 
     // Make objects for reading, parsing, and writing json:
     val objectMapper = ObjectMapper()
     val prettyPrinter = objectMapper.writerWithDefaultPrettyPrinter()
     val textSource = makeTextSource(params)
 
-    for (projectDir in inputJsonDirectory.listFiles()) {
-        if (!projectDir.isDirectory()) {
-            continue
-        }
-        val projectOutDir = File(outputDirectory, projectDir.name)
-        projectOutDir.mkdir()
-        for (jsonFile in projectDir.listFiles()) {
+    inputJsonDirectory.walk().filter { it.isFile }.forEach { jsonFile ->
+        val filename = jsonFile.name
+        if (filename.contains(Regex("[\b_]ENG[\b_]"))) {
+            // The project directory is the path to this file with the input directory components
+            // stripped off the front, then joined with the desired output directory.
+            val projectOutDir = Paths.get(
+                    outputDirectory.toString(),
+                    jsonFile.toPath().removePrefixPath(inputJsonDirectory.toPath()).toString()
+            ).parent.toAbsolutePath()
+            if (Files.notExists(projectOutDir)) {
+                Files.createDirectories(projectOutDir)
+            }
             val jsonTree = objectMapper.readTree(jsonFile)
             // First 21 characters of the filename are the document id
             // Example filename is AFP_ENG_19960918.0012-admin.json
-            val docID = Symbol.from(jsonFile.name.substring(0, 21))
+            val docID = Symbol.from(filename.substring(0, 21))
             val text = textSource.getOriginalText(docID).orNull()
                     ?: throw RuntimeException("Could not get original text for $docID")
             jsonTree.replaceFieldEverywhere("sofaString", text)
-            val outFile = File(projectOutDir, jsonFile.name)
+            val outFile = File(projectOutDir.toString(), filename)
             outFile.writeBytes(prettyPrinter.writeValueAsBytes(jsonTree))
+            logger.info { "Restored $filename" }
+        } else {
+            logger.warn { "Cannot restore $filename, only English is supported" }
         }
     }
 }
@@ -79,4 +89,11 @@ fun docIDToFilename(symbol: Symbol?): String {
     val dir = st?.substring(0, 7) //
     val basename = st?.substring(0, 14)
     return Paths.get(dir, basename).toString()
+}
+
+private fun Path.removePrefixPath(prefix: Path): Path {
+    require(this.startsWith(prefix)) {
+        "The path $this does not start with the prefix $prefix"
+    }
+    return this.subpath(prefix.nameCount, this.nameCount)
 }
