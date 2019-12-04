47ca98a8e8c113280149ad4e6761b8d1e06cef23
Liz Lee <elgclee@gmail.com>
2019-12-04 17:07:43 -0500

    Add parameter allowing unzipped documents

--- gigaword-indexer/src/main/java/edu/isi/vista/gigawordIndexer/ConcatenatedGigawordDocuments.java
+++ gigaword-indexer/src/main/java/edu/isi/vista/gigawordIndexer/ConcatenatedGigawordDocuments.java
@@ -1,11 +1,14 @@
 package edu.isi.vista.gigawordIndexer;
 
 import com.google.common.collect.AbstractIterator;
+import com.google.common.io.ByteSource;
 import edu.isi.nlp.io.GZIPByteSource;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Objects;
 import java.util.regex.Matcher;
@@ -26,6 +29,10 @@ public class ConcatenatedGigawordDocuments implements ArticleSource {
   public static ConcatenatedGigawordDocuments fromGigwordGZippedFile(Path p) throws IOException {
     return new ConcatenatedGigawordDocuments(GZIPByteSource.fromCompressed(p.toFile())
             .asCharSource(StandardCharsets.UTF_8).read());
+  }
+
+  public static ConcatenatedGigawordDocuments fromGigawordFile(Path p) throws  IOException {
+    return new ConcatenatedGigawordDocuments(new String(Files.readAllBytes(p)));
   }
 
   public Iterator<Article> iterator() {
